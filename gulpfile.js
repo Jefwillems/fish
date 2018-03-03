6fa25c40ad1164d70f3826041e582cb46a83a3cf
Jefwillems <jefwillems100@gmail.com>
2018-03-03 23:49:15 +0100

    new gulp build, which copies over assets

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,11 @@ var useref = require("gulp-useref");
 var gulpif = require("gulp-if");
 var minifyCss = require("gulp-clean-css");
 
-gulp.task("build", function() {
+gulp.task("assets", function() {
+  return gulp.src("./assets/**/*").pipe(gulp.dest("./docs/assets/"));
+});
+
+gulp.task("build", ["assets"], function() {
   return gulp
     .src("./index.html")
     .pipe(useref())
