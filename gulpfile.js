8d0a374c7aff6764be000bf0b1350a1dcc2634ee
Jefwillems <jefwillems100@gmail.com>
2018-03-03 20:49:51 +0100

    add gulp configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,20 @@
 var gulp = require("gulp");
 var deploy = require("gulp-gh-pages");
+var useref = require("gulp-useref");
+var gulpif = require("gulp-if");
+var minifyCss = require("gulp-clean-css");
+
+gulp.task("build", function() {
+  return gulp
+    .src("./index.html")
+    .pipe(useref())
+    .pipe(gulpif("*.css", minifyCss()))
+    .pipe(gulp.dest("./dist/"));
+});
 
 /**
  * Push build to gh-pages
  */
-gulp.task("deploy", function() {
+gulp.task("deploy", ["build"], function() {
   return gulp.src("./dist/**/*").pipe(deploy());
 });
