d86e37fe8bb54958b11efb1b66045e8e2f4fc98e
Jefwillems <jefwillems100@gmail.com>
2018-03-13 15:54:36 +0100

    drawing menu buttons

--- js/menu.js
+++ js/menu.js
@@ -3,4 +3,8 @@ function Menu(gameState) {
   this.buttons = [];
   var b = new MenuButton();
 }
-Menu.prototype.draw = function() {};
+Menu.prototype.draw = function() {
+  for (var i = 0; i < this.buttons.length; i++) {
+    this.buttons[i].draw();
+  }
+};
