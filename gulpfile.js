81fd34cab6febbcd2814c688af1141e9baedf168
Jefwillems <jefwillems100@gmail.com>
2018-04-06 00:41:10 +0200

    add prod build

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require("gulp");
 var useref = require("gulp-useref");
 var gulpif = require("gulp-if");
 var minifyCss = require("gulp-clean-css");
+var replace = require("gulp-replace");
 
 gulp.task("assets", function() {
   return gulp.src("./assets/**/*").pipe(gulp.dest("./docs/assets/"));
@@ -13,4 +14,19 @@ gulp.task("build", ["assets"], function() {
     .pipe(useref())
     .pipe(gulpif("*.css", minifyCss()))
     .pipe(gulp.dest("./docs/"));
+});
+
+gulp.task("prod-assets", function() {
+  return gulp.src("./assets/**/*").pipe(gulp.dest("./dist/assets/"));
+});
+
+gulp.task("prod", ["prod-assets"], function() {
+  return gulp
+    .src("./index.html")
+    .pipe(useref())
+    .pipe(gulpif("*.css", minifyCss()))
+    .pipe(replace("assets/", "/static/main/"))
+    .pipe(replace("css/", "/static/main/css/"))
+    .pipe(replace("scripts/", "/static/main/js/"))
+    .pipe(gulp.dest("./dist/"));
 });
